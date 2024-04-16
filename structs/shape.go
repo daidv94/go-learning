d8e224d6b6547a04c33359b49dc1ae246a2f0558
daidv94 <dai.dao@noreply.github.com>
2024-04-16 22:27:09 +0700

    feat(struct): add method for struct and write test about it
    Signed-off-by: daidv94 <dai.dao@noreply.github.com>

--- structs/shape.go
+++ structs/shape.go
@@ -1,8 +1,24 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+// Area method for Rectangle type
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+// Area method for Rectangle type
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
 }
 
 func Perimeter(r Rectangle) float64 {
